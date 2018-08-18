b71095375e74726957792997eda0279b05e9deef
Zach Bryan <zachx1@gmail.com>
2018-08-17 18:24:03 -0700

    mod to package.json

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { render } from "react-dom";
 import "./index.css";
-import App from "./App";
 import registerServiceWorker from "./registerServiceWorker";
 import NovelPicker from "./Components/NovelPicker";
 

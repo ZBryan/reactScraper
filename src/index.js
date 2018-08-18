9822431b4e5a028840c7806f930038c75bb525a0
Zach Bryan <zachx1@gmail.com>
2018-08-17 19:21:44 -0700

    fucking env

--- src/index.js
+++ src/index.js
@@ -2,9 +2,9 @@ import React from "react";
 import { render } from "react-dom";
 import "./index.css";
 import Router from "./Components/Router";
-import { config } from "dotenv";
+// import { config } from "dotenv";
 import registerServiceWorker from "./registerServiceWorker";
-config();
+// config();
 
 render(<Router />, document.getElementById("main"));
 registerServiceWorker();

eef8e28256ff2edb1449ef87c6a801c044354bf5
Zach Bryan <zachx1@gmail.com>
2018-09-01 09:34:47 -0700

    removed comment

--- src/Components/base.js
+++ src/Components/base.js
@@ -3,12 +3,11 @@ import firebase from "firebase/app";
 import "firebase/app";
 import "firebase/auth";
 import "firebase/database";
+
 const apiKey = process.env.REACT_APP_FIREBASE_API_KEY;
 const authDomain = process.env.REACT_APP_FIREBASE_AUTH_DOMAIN;
 const databaseURL = process.env.REACT_APP_FIREBASE_DATABASE_URL;
-console.log("api", apiKey);
-console.log("auth", authDomain);
-console.log("databaseURL", databaseURL);
+
 const firebaseApp = firebase.initializeApp({
   apiKey,
   authDomain,
